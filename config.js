b8e76b8276fa3a23fafc58bd4c6e93372afa73a3
sulikdev <63726649+sulikdev@users.noreply.github.com>
2025-05-01 13:55:43 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  botToken: '7282077489:AAFn7QFdRtwhgf5QTYg9FFIvioatCnmeS-E',
+  adminId: '5154775313', // ID администратора
+  specialAdminId: '5154775313', // ID специального администратора
+  helpChatId: '-4785764272', // ID чата для помощников
+  reportChatId: '-4785764272' // ID чата для репортов
+};
