46a035ff186bbbbdd94fa8d0d16a06e475cfe648
sulikdev <63726649+sulikdev@users.noreply.github.com>
2025-05-01 16:54:02 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  BOT_TOKEN: '7282077489:AAFn7QFdRtwhgf5QTYg9FFIvioatCnmeS-E',
-  ADMIN_CHAT_ID: '-4785764272',
-  SUPPORT_CHAT_ID: '-4785764272',
-  DB_FILE: './db.json'
+  botToken: '7282077489:AAFn7QFdRtwhgf5QTYg9FFIvioatCnmeS-E',
+  adminId: '5154775313', // ID администратора
+  specialAdminId: '5154775313', // ID специального администратора
+  helpChatId: '-4785764272', // ID чата для помощников
+  reportChatId: '-4785764272' // ID чата для репортов
 };
